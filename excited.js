7ed533f2cddcd7b1484e925ac0a6adfdbb2d9c0c
Harshit Agarwal <harshitagarwal49@gmail.com>
2017-10-29 11:48:53 +0530

    Update excited.js

--- excited.js
+++ excited.js
@@ -1,14 +1,14 @@
 //alert("Connected");
-var angry_link1 = "https://www.youtube.com/watch?v=OPf0YbXqDm0";
-var angry_link2 = "https://www.youtube.com/watch?v=YykjpeuMNEk";
-var angry_link3 = "https://www.youtube.com/watch?v=2vjPBrBU-TM";
-var angry_link4 = "https://www.youtube.com/watch?v=HMUDVMiITOU";
-var angry_link5 = "https://www.youtube.com/watch?v=7zp1TbLFPp8";
-var angry_link6 = "https://www.youtube.com/watch?v=7-7knsP2n5w";
-var angry_link7 = "https://www.youtube.com/watch?v=hT_nvWreIhg";
-var angry_link8 = "https://www.youtube.com/watch?v=34Na4j8AVgA";
-var angry_link9 = "https://www.youtube.com/watch?v=WTJSt4wP2ME";
-var angry_link10 = "https://www.youtube.com/watch?v=v6IAJOOmDMg";
+var angry_link1 = "https://www.youtube.com/embed/OPf0YbXqDm0";
+var angry_link2 = "https://www.youtube.com/embed/YykjpeuMNEk";
+var angry_link3 = "https://www.youtube.com/embed/2vjPBrBU-TM";
+var angry_link4 = "https://www.youtube.com/embed/HMUDVMiITOU";
+var angry_link5 = "https://www.youtube.com/embed/7zp1TbLFPp8";
+var angry_link6 = "https://www.youtube.com/embed/7-7knsP2n5w";
+var angry_link7 = "https://www.youtube.com/embed/hT_nvWreIhg";
+var angry_link8 = "https://www.youtube.com/embed/34Na4j8AVgA";
+var angry_link9 = "https://www.youtube.com/embed/WTJSt4wP2ME";
+var angry_link10 = "https://www.youtube.com/embed/v6IAJOOmDMg";
 
 var angry = [angry_link1, angry_link2, angry_link3, angry_link4, angry_link5, angry_link6, angry_link7, angry_link8, angry_link9,angry_link10];
 
