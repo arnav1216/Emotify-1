7a33adb40c686aba03c15dea92a8e4d060ab6a82
Harshit Agarwal <harshitagarwal49@gmail.com>
2017-10-29 11:28:50 +0530

    Add files via upload

---
+++ happy.js
@@ -0,0 +1,31 @@
+//alert("Connected");
+var angry_link1 = "https://www.youtube.com/embed/dT2owtxkU8k&index=2&list=PLinS5uF49IBo8HLKBDAjQaeiN4TjHi75Q";
+var angry_link2 = "https://www.youtube.com/embed/1-3exHGBtOg";
+var angry_link3 = "https://www.youtube.com/embed/nXcHRO_H-Yo";
+var angry_link4 = "https://www.youtube.com/embed/Q-GLuydiMe4";
+var angry_link5 = "https://www.youtube.com/embed/kOkQ4T5WO9E";
+var angry_link6 = "https://www.youtube.com/embed/FM7MFYoylVs";
+var angry_link7 = "https://www.youtube.com/embed/Ho32Oh6b4jc";
+var angry_link8 = "https://www.youtube.com/embed/39_OmBO9jVg";
+var angry_link9 = "https://www.youtube.com/embed/PT2_F-1esPk";
+var angry_link10 = "https://www.youtube.com/embed/AbPED9bisSc";
+
+var angry = [angry_link1, angry_link2, angry_link3, angry_link4, angry_link5, angry_link6, angry_link7, angry_link8, angry_link9];
+
+var one;
+one = Math.random()*10;
+one1 = Math.floor(one);
+
+document.getElementById("yoo1").setAttribute("src",angry[one1]);
+
+var two;
+two = Math.random()*10;
+two2 = Math.floor(two);
+
+document.getElementById("yoo2").setAttribute("src",angry[two2]);
+
+var three;
+three = Math.random()*10;
+three3 = Math.floor(three);
+
+document.getElementById("yoo3").setAttribute("src",angry[three3]);
